a9179cd76d1a1d693d6a11b1e4ea2b831b3b7350
Scott Vitale <svvitale@gmail.com>
2015-02-15 23:35:55 -0700

    Add the rest of the routes.  Still missing the member view and tests for the views.

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import patterns, url
 
-from chat.views import RoomView
+from chat.views import RoomView, UserView, MessageView
 
 urlpatterns = patterns('',
     (r'^rooms/$', RoomView.as_view()),
-    (r'^rooms/(?P<room_id>\d+)/$', RoomView.as_view()),
+    (r'^rooms/(?P<item_id>\d+)/$', RoomView.as_view()),
+    (r'^users/$', UserView.as_view()),
+    (r'^users/(?P<item_id>\d+)/$', UserView.as_view()),
+    (r'^rooms/(?P<room_id>)/messages/$', MessageView.as_view()),
+    (r'^rooms/(?P<room_id>)/messages/(?P<item_id>)$', MessageView.as_view()),
+    #(r'^rooms/(?P<room_id>)/members/$', MemberView.as_view()),
+    #(r'^rooms/(?P<room_id>)/members/(?P<item_id>)$', MemberView.as_view()),
 )
